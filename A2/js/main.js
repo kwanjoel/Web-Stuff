ae291ea2378a30b24a88e73b7985dc83acc515d4
Joel Kwan <jkwan33@myseneca.ca>
2018-01-28 15:58:42 -0500

    lots of finished functions

--- A2/js/main.js
+++ A2/js/main.js
@@ -8,51 +8,99 @@
 *
 *
 ********************************************************************************/
-
+var employeesModel = [];
 $(function () { //Ready handler\
     console.log("jQuery ready");
 
-    var employeesModel= [];
+
+    //Fetch data and populate table
     initializeEmployeesModel();
 
-    function initializeEmployeesModel() {
-        $.ajax({
-            url: "https://web422teamapi.herokuapp.com/employees",
-            type: "GET",
-            contentType: "application/json"
-        })
+    //keyup event for search field
+    $("#employee-search").keyup(function() {
+        getFilteredEmployeesModel($("#employee-search").val())
+    })
+
+    //Clicking a body row
+    $(".bootstrap-header-table").on('click', '.body-row', function () {
+        let currentEmployee = getEmployeeModelById($(this).attr('data-id'));
+        
+        
+    })
+});
+
+
+function initializeEmployeesModel() {
+    $.ajax({
+        url: "https://web422teamapi.herokuapp.com/employees",
+        type: "GET",
+        contentType: "application/json"
+    })
         .done(function (employees) {
             employeesModel = _.take(employees, 300);
             refreshEmployeeRows(employeesModel);
         })
         .fail(function () {
             showGenericModal("Error", "Unable to get Employees");
-        }) 
+        })
 
-    }
+}
+
+function refreshEmployeeRows(employees) {
+    console.log("refreshing employee rows...")
+    $("#employees-table").empty();
+
+    var employeeTemplate = _.template(
+        '<% _.forEach(employees, function(employee) { %>' +
+        '<div class="row body-row" data-id="<%- employee._id %>">' +
+        '<div class="col-xs-4 body-column">' + '<%- employee.FirstName %>' + '</div>' +
+        '<div class="col-xs-4 body-column"><%- employee.LastName %></div>' +
+        '<div class="col-xs-4 body-column"><%- employee.Position.PositionName %></div>' +
+        '</div>' +
+        '<% }); %>'
+    );
+    let employeeRow = employeeTemplate({ employees });
+    $("#employees-table").append(employeeRow);
+
+}
+
+function getFilteredEmployeesModel(filterString) {
     
-    function showGenericModal(title, message) {
-        console.log("Testing showGenericModal");
-        $("#genericModal").$("#model-title")(title);
-        $("#genericModal").$("model-body")(message);
+    //Converting to regex 
+    let caseInsensitiveString = new RegExp(filterString, 'i');
+    console.log("Filtering by: " + caseInsensitiveString);
+
+    let filteredEmployees = _.filter(employeesModel, function(employee){
+        if (employee.FirstName.match(caseInsensitiveString) || employee.LastName.match(caseInsensitiveString) || employee.Position.PositionName.match(caseInsensitiveString)) {
+            return employee;
+        }
+    });
+
+    console.log(filteredEmployees);
+
+    if (filterString == "") {
+        refreshEmployeeRows(employeesModel);
     }
 
-    function refreshEmployeeRows(employees) {
-        console.log("refreshing employee rows...")
-        
-        
-        var employeeTemplate = _.template(
-            '<% _.forEach(employees, function(employee) { %>' +
-                '<div class="row body-row" data-id="<%- employee._id %>">' +
-                    '<div class="col-xs-4 body-column">' + '<%- employee.FirstName %>' + '</div>' +
-                    '<div class="col-xs-4 body-column"><%- employee.LastName %></div>' +
-                    '<div class="col-xs-4 body-column"><%- employee.Position.PositionName %></div>' +
-                '</div>' +
-                '<% }); %>'
-        );
-        let employeeRow = employeeTemplate({employees});
-        $("#employees-table").html(employeeRow);
-        
+    else {
+        refreshEmployeeRows(filteredEmployees);
     }
+}
 
-});
+function showGenericModal(title, message) {
+    console.log("Testing showGenericModal");
+
+    $('.modal-title').empty();
+    $('.modal-body').empty();
+    $('.modal-title').html(title);
+    $('.modal-body').html(message);
+    $('#genericModal').modal('show');
+}
+
+function getEmployeeModelById(id) {
+    let found = _.find(employeesModel, function (employee) {
+        return employee._id == id;
+    })
+
+    return found;
+}
